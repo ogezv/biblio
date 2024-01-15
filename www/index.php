21407e4f388d727319045495befa851d28d7d7dd
victor.ogez@laposte.net <victor.ogez@laposte.net>
2024-01-15 10:35:05 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://bootswatch.com/5/quartz/bootstrap.min.css">
-    <title>Biblio</title>
+    <title>Biblio~</title>
 </head>
 
 <body>
